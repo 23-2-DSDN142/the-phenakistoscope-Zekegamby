808d8f6fb68196f17f861b0111de8edb3a089236
Zekegamby <zekegamby@gmail.com>
2023-09-15 13:52:51 +1200

    Revert "Final"
    This reverts commit a32873e7200e3d5a217d5cd4d071bda063de458b.

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,24 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 25;
+
+const colorPalette = [
+  [26, 40, 71],
+  [252, 230, 198],
+  [106, 154, 224],
+  [30, 61, 97]
+];
+
+let colorIndex = 0;
+let backgroundColorIndex = 2;
+let num = 1;
+let increment = 1;
+
+// Define variables for color shifting
+let color1;
+let color2;
+let color3;
+let lerpedColor;
+let t = 0;
+let lerpingSpeed = 0.0003;
 
 function setup_pScope(pScope) {
   pScope.output_mode(ANIMATED_DISK);
@@ -29,48 +49,101 @@ function setup_layers(pScope) {
   layer3.set_boundary(0, 400);
 }
 
-function setup_layers(pScope){
+function wave(x, y, animation, pScope) {
+  push();
+  scale(animation.frame * 1.6);
+  noStroke();
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  let palette = color(colorPalette[colorIndex]);
+  fill(palette);
+  for (let i = 0; i < 6; i++) {
+    num += increment;
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+    // Check if we need to reverse the pattern
+    if (num == 5 || num == 1) {
+      increment *= -1; // Reverse the increment direction
+    }
+    beginShape();
+    for (let angle = num * 10; angle <= 360; angle += 10) {
+      let xCoord = cos(angle) * (Math.floor(random(100)));
+      let yCoord = sin(angle) * (Math.floor(random(50)) + 30) * cos(animation.frame * (Math.floor(random(100))) + angle);
+      vertex(xCoord, yCoord);
+    }
+    endShape(CLOSE);
+    translate(0, 10);
+  }
 
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+  pop();
+
+  colorIndex = (colorIndex + 1) % colorPalette.length;
 }
 
-function faces(x, y, animation, pScope){
-  
-  scale(animation.frame*2);
+function seagulls(x, y, animation, pScope) {
+  num += increment;
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  // Check if we need to reverse the pattern
+  if (num == 5 || num == 1) {
+    increment *= -1; // Reverse the increment direction
+  }
+  push();
+  translate(num * x, num * y);
+  noStroke();
 
-}
+  // Randomly select a color from the palette for the seagulls
+  let palette = colorPalette[Math.floor(random(colorPalette.length))];
+  fill(palette[0], palette[1], palette[2], 200);
 
-function squares(x, y, animation, pScope){
-
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
-
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+  for (let i = 0; i < 5; i++) {
+    ellipse(i * Math.floor(random(60)), -30 + cos(frameCount * 0.1 + i * 0.3) * 10, 20, 10);
+  }
+  pop();
 
 }
 
 
-// Rest of your code remains unchanged
+function decorativePattern(x, y, animation, pScope) {
+  push();
+  translate(x, y);
 
+  // Decorative pattern
+  let rotationAngle = -300;
+  let numShapes = 25;
+  let angleIncrement = 360 / numShapes;
+  for (let i = 0; i < numShapes; i++) {
+    let angle = radians(i * angleIncrement + rotationAngle);
+    let radius = 100 + 60 * cos(angle * 6); // Varying radius for an exciting effect
+    let shapeX = radius * cos(angle);
+    let shapeY = radius * sin(angle);
 
-// Rest of your code remains unchanged
+    // Calculate the interpolated color
+    lerpedColor = lerpColor(color1, color2, t);
+
+    // Morphing shapes
+    let size = map(sin(angle + animation.frame), -1, 1, 10, 40); // Morphing size
+    let rotation = map(cos(angle + animation.frame), -1, 1, 0, PI); // Morphing rotation
+
+    // Changing colors
+    fill(lerpedColor); // Use the interpolated color
+
+    push();
+    translate(shapeX, shapeY);
+    rotate(rotation);
+    ellipse(0, 0, size, size); // Decorative shapes
+    pop();
+  }
+
+  pop();
+
+  // Increment the interpolation variable
+  t += lerpingSpeed;
+
+  // Wrap t to keep it within [0, 1]
+  if (t > 1) {
+    t = 0;
+    // Swap colors when one interpolation cycle is complete
+    let temp = color1;
+    color1 = color2;
+    color2 = color3;
+    color3 = temp;
+  }
+}
