98cc3ef01b04dbac5804a461edfea9d1ef076e83
Zekegamby <zekegamby@gmail.com>
2023-09-15 13:37:30 +1200

    Final

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,6 @@
 const SLICE_COUNT = 10;
+let rotationAngle = 0;
+let circlePositions = [];
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
@@ -9,41 +11,78 @@ function setup_pScope(pScope){
 }
 
 function setup_layers(pScope){
-
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
-
-  var layer1 = new PLayer(faces);
+  new PLayer(null, 220);  // Background layer
+  var layer1 = new PLayer(centralElement);
   layer1.mode( SWIRL(5) );
   layer1.set_boundary( 200, 1000 );
-
-  var layer2 = new PLayer(squares);
+  var layer2 = new PLayer(decorativePattern);
   layer2.mode( RING );
   layer2.set_boundary( 0, 400 );
 }
 
-function faces(x, y, animation, pScope){
-  
-  scale(animation.frame*2);
+function centralElement(x, y, animation, pScope){
+  push();
+  translate(x, y);
+  rotate(radians(rotationAngle));
+  scale(animation.frame * 2);
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  fill(25, 100, 100); // Red color for central element
+  ellipse(0, 0, 60, 60); // Central rotating element
 
+  pop();
+
+  rotationAngle += 1; // Rotate the central element
 }
 
-function squares(x, y, animation, pScope){
+function decorativePattern(x, y, animation, pScope){
+  push();
+  translate(x, y);
 
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
+  // Decorative pattern
+  let numShapes = 12;
+  let angleIncrement = 360 / numShapes;
+  for (let i = 0; i < numShapes; i++) {
+    let angle = radians(i * angleIncrement + rotationAngle);
+    let radius = 100 + 60 * cos(angle * 6); // Varying radius for an exciting effect
+    let shapeX = radius * cos(angle);
+    let shapeY = radius * sin(angle);
+    
+    // Morphing shapes
+    let size = map(sin(angle + animation.frame), -1, 1, 10, 40); // Morphing size
+    let rotation = map(cos(angle + animation.frame), -1, 1, 0, PI); // Morphing rotation
+    
+    // Changing colors
+    let hue = (i * 30 * 10) % 360; // Changing hue
+    fill(hue, 10, 80); // Random hue, full saturation, and brightness
+    
+    push();
+    translate(shapeX, shapeY);
+    rotate(rotation);
+    ellipse(0, 0, size, size); // Decorative shapes
+    pop();
+  }
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  // Move circles to the outer ring while rotating around the center
+  for (let i = 0; i < circlePositions.length; i++) {
+    let pos = circlePositions[i];
+    let angle = radians(rotationAngle + i * (360 / circlePositions.length));
+    let radius = 120 + 40 * cos(angle * 3); // Varying radius for rotation
+    let x = radius * cos(angle);
+    let y = radius * sin(angle);
+    ellipse(x + pos.x, y + pos.y, 70, 20); // Moving circles
+  }
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-
+  pop();
 }
+
+function updateCirclePositions() {
+  // Update the positions of circles moving to the outer ring
+  circlePositions = [];
+  let numCircles = 8;
+  for (let i = 0; i < numCircles; i++) {
+    circlePositions.push({ x: 0, y: 0 });
+  }
+}
+
+// Call updateCirclePositions to initialize the circle positions
+updateCirclePositions();
